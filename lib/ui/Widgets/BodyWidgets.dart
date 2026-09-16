dcfe679f245afd722452a6af44f9dea51623edd8
Kinocoder <nnnnguyentuankiet@gmail.com>
2026-09-16 09:39:28 +0700

    feat: Container Box

--- lib/ui/Widgets/BodyWidgets.dart
+++ lib/ui/Widgets/BodyWidgets.dart
@@ -6,12 +6,24 @@ class BodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: 
         /*child:Text("Hello every one!",
           style: TextStyle(fontSize:50,fontWeight:FontWeight.bold ,color: Colors.cyan),
         )*/
-       child: Image.asset('asserts/images/1789480835463_8789316428734258228_g2905689298196080542_c6d63fbdcc49397cda46f6daa14250e6.jpg'),
-    )
+        Column(
+          children: [
+            Container(
+               width: 500,
+               height: 500,
+               child: Image.asset('asserts/images/1789480835463_8789316428734258228_g2905689298196080542_c6d63fbdcc49397cda46f6daa14250e6.jpg',
+                 fit:BoxFit.fill ,
+               )),
+            Text('name: Yến '),
+            Text('price: 1.000.000VND'),
+            Text('description: Hơi báo và rất láo')
+          ],
+        ),
+    
     );
   }
 }
