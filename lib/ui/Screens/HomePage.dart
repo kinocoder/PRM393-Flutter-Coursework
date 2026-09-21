081dd052d840393c0351fa711ddbb384a880c704
Kinocoder <nnnnguyentuankiet@gmail.com>
2026-09-21 15:51:58 +0700

    feat: Bài tập Lap 4

--- lib/ui/Screens/HomePage.dart
+++ lib/ui/Screens/HomePage.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:hoc_tren_truong/ui/Widgets/BodyWidgets.dart';
 
+import '../Widgets/Lap4Widget.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
-  @override
+  static const titles = [
+    'Giao diện trên lớp',
+    'Lab 4 – Flutter UI Fundamentals',
+    'Đang cập nhật...',
+  ];
+
+  //Thanh công cụ
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(length: titles.length, child: Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.menu),
-        title: const Text("Home Page"),
-        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.search))],
+        title: Builder(builder: (context) {
+          final controler = DefaultTabController.of(context);
+
+          return AnimatedBuilder(
+            animation: controler, builder: (context, child) {
+            return Text(titles[controler.index],
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            );
+          },);
+        },),
+        bottom: const TabBar(tabs: [
+          Tab(icon: Icon(Icons.menu_book), text: "Giao diện trên lớp",),
+          Tab(
+            icon: Icon(Icons.menu_book), text: "Lab 4 - Flutter UI Fundament",),
+          Tab(icon: Icon(Icons.lock_clock), text: "Đang cập nhật..",)
+        ]),
       ),
-      body: BodyWidget()
+      body: TabBarView(children: [
+        BodyWidget(),
+        LapExercise(),
+        const Center(child: Text("Trang hiện nay đang cập nhật"),)
+      ]),
+    )
     );
   }
+
+
 }
---
+++ lib/ui/Widgets/Lap4Widget.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:hoc_tren_truong/ui/Screens/core_widget_demo.dart';
+
+class LapExercise extends StatelessWidget {
+  const LapExercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(16),
+      children: [
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Text(
+                    style: TextStyle(fontSize: 25),
+                    "Exercise 1 - Core Widgets Demo",
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: IconButton(
+                    icon: const Icon(Icons.chevron_right),
+                    iconSize: 35,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => const CoreWidgetDemo() as Widget));
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Text(
+                    style: TextStyle(fontSize: 25),
+                    "Exercise 2 - Input Controls Demo",
+                  ),
+                ),
+                Expanded(flex: 1, child: Icon(size: 35, Icons.chevron_right)),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Text(
+                    style: TextStyle(fontSize: 25),
+                    "Exercise 3 - Layout Demo",
+                  ),
+                ),
+                Expanded(flex: 1, child: Icon(size: 35, Icons.chevron_right)),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Text(
+                    style: TextStyle(fontSize: 25),
+                    "Exercise 4 - App Structure & Theme",
+                  ),
+                ),
+                Expanded(flex: 1, child: Icon(size: 35, Icons.chevron_right)),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Text(
+                    style: TextStyle(fontSize: 25),
+                    "Exercise 5 - Common UI Fixes",
+                  ),
+                ),
+                Expanded(flex: 1, child: Icon(size: 35, Icons.chevron_right)),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
