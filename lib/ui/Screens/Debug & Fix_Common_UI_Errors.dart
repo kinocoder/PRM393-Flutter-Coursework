896d2e4c5d0f2a6d21de0f6348b1db1c9f9e8728
Kinocoder <nnnnguyentuankiet@gmail.com>
2026-09-26 11:53:20 +0700

    fix: Sửa lỗi bằng cách bọc SingleChildScrollView vào phần bị tràn để có thể cuộn xuống

--- lib/ui/Screens/Debug & Fix_Common_UI_Errors.dart
+++ lib/ui/Screens/Debug & Fix_Common_UI_Errors.dart
@@ -7,27 +7,29 @@ class CommonUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Debug & Fix Common UI Errors")),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Correct ListView inside Column using Expanded",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          Container(
-            height: 1000,
-            child: Expanded(
-              child: ListView(
-                children: [
-                  ListTile(leading: Icon(Icons.movie), title: Text('Movie A')),
-                  ListTile(leading: Icon(Icons.movie), title: Text('Movie B')),
-                  ListTile(leading: Icon(Icons.movie), title: Text('Movie C')),
-                  ListTile(leading: Icon(Icons.movie), title: Text('Movie D')),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Correct ListView inside Column using Expanded",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              height: 1000,
+              child: Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(leading: Icon(Icons.movie), title: Text('Movie A')),
+                    ListTile(leading: Icon(Icons.movie), title: Text('Movie B')),
+                    ListTile(leading: Icon(Icons.movie), title: Text('Movie C')),
+                    ListTile(leading: Icon(Icons.movie), title: Text('Movie D')),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
